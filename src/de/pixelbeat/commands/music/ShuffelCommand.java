3e5f04888a9e5db8b41be55ae70e24f879fae8fe
Nebalus <56486387+Nebalus@users.noreply.github.com>
2021-04-20 19:03:21 +0200

    Adding new command: Shuffel

--- src/de/pixelbeat/commands/music/ShuffelCommand.java
+++ src/de/pixelbeat/commands/music/ShuffelCommand.java
@@ -1,6 +1,9 @@
 package de.pixelbeat.commands.music;
 
+import de.pixelbeat.PixelBeat;
 import de.pixelbeat.commands.types.ServerCommand;
+import de.pixelbeat.music.MusicController;
+import de.pixelbeat.music.Queue;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
@@ -9,8 +12,11 @@ public class ShuffelCommand implements ServerCommand{
 
 	@Override
 	public void performCommand(Member m, TextChannel channel, Message message) {
-		// TODO Auto-generated method stub
-		
+		MusicController controller = PixelBeat.INSTANCE.playerManager.getController(channel.getGuild().getIdLong());
+		Queue queue = controller.getQueue();
+		if(queue.getQueuelist().size() > 1) {
+			queue.shuffel(); 
+			channel.sendMessage(queue.getQueuelist().size()+" tracks has been shuffeled!").queue();
+		}
 	}
-
 }
