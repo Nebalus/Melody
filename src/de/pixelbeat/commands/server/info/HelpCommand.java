f6856e32a9fd9399f561879661e18f988e0edb61
Aiden N <56486387+Nebalus@users.noreply.github.com>
2021-05-16 22:47:13 +0200

    Add files via upload

---
+++ src/de/pixelbeat/commands/server/info/HelpCommand.java
@@ -0,0 +1,5 @@
+package de.pixelbeat.commands.server.info;
+
+public class HelpCommand {
+
+}
---
+++ src/de/pixelbeat/commands/server/music/JoinCommand.java
@@ -0,0 +1,41 @@
+package de.pixelbeat.commands.server.music;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+
+import de.pixelbeat.PixelBeat;
+import de.pixelbeat.commands.types.ServerCommand;
+import de.pixelbeat.music.MusicController;
+import de.pixelbeat.music.MusicUtil;
+import de.pixelbeat.speechpackets.MessageFormatter;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.VoiceChannel;
+import net.dv8tion.jda.api.managers.AudioManager;
+
+public class JoinCommand implements ServerCommand{
+	
+	private PixelBeat pixelbeat = PixelBeat.INSTANCE;
+	private MessageFormatter mf = pixelbeat.getMessageFormatter();
+	
+	@Override
+	public void performCommand(Member m, TextChannel channel, Message message, Guild guild) {
+		pixelbeat.entityManager.getGuildEntity(guild.getIdLong()).setChannelId(channel.getIdLong());
+		GuildVoiceState state;
+		VoiceChannel vc;
+		if((state = m.getVoiceState()) != null && (vc = state.getChannel()) != null) {
+			AudioManager manager = guild.getAudioManager();
+			manager.openAudioConnection(vc);
+			MusicController controller = pixelbeat.playerManager.getController(guild.getIdLong());
+			AudioPlayer player = controller.getPlayer();
+			player.setPaused(false);
+			if(player.getPlayingTrack() == null) {
+				controller.setAfkTime(600);
+			}
+		}else {
+			MusicUtil.sendEmbledError(guild.getIdLong(), mf.format(guild.getIdLong(), "feedback.music.user-not-in-vc"));		
+		}
+	}
+}
