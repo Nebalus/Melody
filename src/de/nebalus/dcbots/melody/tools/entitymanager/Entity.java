b88a0ef0e9c317e6336b407b8a977f023f30a173
Aiden N <aiden.nygaard@outlook.de>
2022-05-04 01:14:49 +0200

    Revert "EntityManager rework"
    This reverts commit 4b910f1e457ed25ede691c5ea58b4258b1497f46.

--- src/de/nebalus/dcbots/melody/tools/entitymanager/Entity.java
+++ src/de/nebalus/dcbots/melody/tools/entitymanager/Entity.java
@@ -1,88 +1,35 @@
 package de.nebalus.dcbots.melody.tools.entitymanager;
 
-import java.util.concurrent.ConcurrentHashMap;
-
 import de.nebalus.dcbots.melody.core.constants.Settings;
 
-public class Entity
-{
+public class Entity {
 
 	private Long expiretime;
 	private Boolean needtoexport;
 	
-	private final ConcurrentHashMap<String, DatabaseValueContainer> dbvalues;
-	
-	protected Entity() 
-	{
+	protected Entity() {
 		this.expiretime = System.currentTimeMillis() + Settings.ENTITY_EXPIRE_TIME;
-		this.needtoexport = false;
-		
-		this.dbvalues = new ConcurrentHashMap<String, DatabaseValueContainer>();
-	}
-	 
-	protected void updateDatabaseValue(String valuename, Object value) 
-	{	
-		DatabaseValueContainer dvc = getDatabaseValueContainer(valuename);
-		dvc.updateValue(value);
-		setNeedToExport(true);
+		this.needtoexport = true;
 	}
 	
-	protected void updateDatabaseValue(DatabaseValueContainer dvc, Object value) 
-	{
-		dvc.updateValue(value);
-		setNeedToExport(true);
-	}
-	
-	protected Object getDatabaseValue(String valuename) 
-	{
-		DatabaseValueContainer dvc = getDatabaseValueContainer(valuename);
-		return dvc.getValue();
-	}
-	
-	protected Object getDatabaseValue(DatabaseValueContainer dvc) 
-	{
-		return dvc.getValue();
-	}
-	
-	protected DatabaseValueContainer getDatabaseValueContainer(String valuename) 
-	{
-		if(dbvalues.containsKey(valuename)) 
-		{
-			DatabaseValueContainer dvc = dbvalues.get(valuename);
-			return dvc;
-		}
-		else
-		{
-			throw new NullPointerException("The DatabaseValueContainer ID:" + valuename + " does not exist!");
-		}
-	}
-	
-	protected boolean createDatabaseValueContainer(DatabaseValueContainer newdvc) 
-	{
-		if(!dbvalues.containsKey(newdvc.getKey())) {
-			dbvalues.put(newdvc.getKey(), newdvc);
-			return true;
-		}
-		return false;
-	}
-	
-	public Long getExpireTime() 
-	{
+	public Long getExpireTime() {
 		return this.expiretime;
 	}
 	
-	public boolean needToExport() 
-	{
-		return this.needtoexport;
+	public boolean needToExport() {
+		return needtoexport;
 	}
 	
-	protected void setNeedToExport(boolean value) 
-	{
-		this.needtoexport = value;
+	protected void setNeedToExport(boolean value) {
+		needtoexport = value;
 	}
-
-	protected void renewExpireTime() 
-	{
+	
+	protected void update() {
+		this.needtoexport = true;
+		renewExpireTime();
+	}
+	
+	protected void renewExpireTime() {
 		this.expiretime = System.currentTimeMillis() + Settings.ENTITY_EXPIRE_TIME;
 	}
 }
