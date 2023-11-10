2858086f22de773dfd4b17de4b98047a819acc30
Nebalus <56486387+Nebalus@users.noreply.github.com>
2023-11-10 20:26:05 +0100

    .

--- src/de/nebalus/dcbots/melody/MelodyBotInstance.java
+++ src/de/nebalus/dcbots/melody/MelodyBotInstance.java
@@ -7,6 +7,8 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
@@ -25,14 +27,26 @@ public class MelodyBotInstance extends DCBotInstance {
 				GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_VOICE_STATES);
 
 		builder.setToken("");
-		builder.setStatus(OnlineStatus.DO_NOT_DISTURB);
-		builder.setActivity(Activity.playing("booting myself..."));
+		builder.setStatus(OnlineStatus.OFFLINE);
+		builder.setActivity(Activity.playing("Relaunch coming soon!"));
 		builder.setMemberCachePolicy(MemberCachePolicy.VOICE.or(MemberCachePolicy.ONLINE));
 		builder.setChunkingFilter(ChunkingFilter.NONE);
 		builder.setLargeThreshold(50);
 		builder.addEventListeners();
 
 		finalizeJDABuildProcess(builder);
+		
+		MelodyCore.getMelodyApp().getLogger().log("Melodys connection was succesfully build");
+
+		// Print some information about the bot
+		MelodyCore.getMelodyApp().getLogger().log("Melody is connected as '" + getJDA().getSelfUser().getEffectiveName() + "'");
+		MelodyCore.getMelodyApp().getLogger().log("The bot is present on these guilds:");
+		for (Guild guild : getJDA().getGuildCache()) {
+			MelodyCore.getMelodyApp().getLogger().log("\t- " + guild.getName() + " (" + guild.getId() + ") (" + guild.getMembers().size() + " Members)");
+			for(Member member : guild.getMembers()) {
+				MelodyCore.getMelodyApp().getLogger().log("\t\t- " + member.getEffectiveName());
+			}
+		}
 	}
 
 	@Override
