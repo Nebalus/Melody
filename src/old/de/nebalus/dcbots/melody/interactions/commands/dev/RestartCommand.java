87da0da5ae7626943c51e42c5a032f7c7e4a93e7
Nebalus <56486387+Nebalus@users.noreply.github.com>
2023-11-13 15:55:44 +0100

    Removed Depricated Files

--- src/old/de/nebalus/dcbots/melody/interactions/commands/dev/RestartCommand.java
+++
@@ -1,5 +0,0 @@
-package old.de.nebalus.dcbots.melody.interactions.commands.dev;
-
-public class RestartCommand {
-
-}
--- src/old/de/nebalus/dcbots/melody/tools/cmdbuilder/PermissionGroup.java
+++
@@ -1,5 +0,0 @@
-package old.de.nebalus.dcbots.melody.tools.cmdbuilder;
-
-public enum PermissionGroup {
-	DEVELOPER, ADMIN, DJ, EVERYONE;
-}
